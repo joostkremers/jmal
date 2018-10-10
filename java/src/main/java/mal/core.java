997985fbd7af66151dcf6356a4d3cf694bbb2483
Joost Kremers <joostkremers@fastmail.fm>
2018-10-10 21:04:19 +0200

    Implement arguments to assert the number of arguments.

--- java/src/main/java/mal/core.java
+++ java/src/main/java/mal/core.java
@@ -15,10 +15,59 @@ public class core {
     // Although these functions are obvious candidates for reduce(), the fact that
     // we need to handle errors makes that too cumbersome.
 
-    private static int checkMalInt(MalType arg) throws MalException {
-        if (arg instanceof MalInt)
-            return (int)arg.getJValue();
-        else throw new MalException("Wrong argument type: expected int, got " + arg.getType() + ".");
+    /**
+     * Check the number of arguments passed to a function.
+     * <p>
+     * This function checks that the argument list has exactly the specified
+     * number of arguments.
+     *
+     * @param list The list of arguments to be checked.
+     * @param n The number of arguments required.
+     */
+    private static void assertNArgs(MalList list, int n) throws MalException {
+        int size = list.getJValue().size();
+
+        if (size != n) {
+            String errormsg = String.format("Wrong number of arguments: required %d, received %d.", n, size);
+            throw new MalException(errormsg);
+        }
+    }
+
+    /**
+     * Check the number of arguments passed to a function.
+     * <p>
+     * This function checks that the number of arguments in the list falls
+     * between the bounds given.
+     *
+     * @param list The list of arguments to be checked.
+     * @param min The minimum number of arguments required.
+     * @param max The maximum number of arguments allowed.
+     */
+    private static void assertNArgs(MalList list, int min, int max) throws MalException {
+        int size = list.getJValue().size();
+
+        if ((size < min) || (size > max)) {
+            String errormsg = String.format("Wrong number of arguments: required %d-%d, received %d.", min, max, size);
+            throw new MalException(errormsg);
+        }
+    }
+
+    /**
+     * Check the number of arguments passed to a function.
+     * <p>
+     * This function checks that the argument list has at least the minimum
+     * number of arguments required. There is no upper bound.
+     *
+     * @param list The list of arguments to be checked.
+     * @param min The minimum number of arguments required.
+     */
+    private static void assertMinArgs(MalList list, int min) throws MalException {
+        int size = list.getJValue().size();
+
+        if (size < min) {
+            String errormsg = String.format("Wrong number of arguments: required at least %d, received %d.", min, size);
+            throw new MalException(errormsg);
+        }
     }
 
     static MalFunction malAdd = new MalFunction() {
@@ -27,7 +76,7 @@ public class core {
                 int result = 0;
 
                 for(MalType i : args.getJValue()) {
-                    result += checkMalInt(i);
+                    result += i.assertType(MalInt.class).getJValue();
                 }
                 return new MalInt(result);
             }
@@ -40,11 +89,11 @@ public class core {
 
                 if (size == 0) return new MalInt(0);
 
-                int result = checkMalInt(args.get(0));
+                int result = args.get(0).assertType(MalInt.class).getJValue();
                 if (size == 1) return new MalInt(-result);
 
                 for (MalType i : args.getJValue().subList(1,size)) {
-                    result -= checkMalInt(i);
+                    result -= i.assertType(MalInt.class).getJValue();
                 }
                 return new MalInt(result);
             }
@@ -56,7 +105,7 @@ public class core {
                 int result = 1;
 
                 for(MalType i : args.getJValue()) {
-                    result *= checkMalInt(i);
+                    result *= i.assertType(MalInt.class).getJValue();
                 }
                 return new MalInt(result);
             }
@@ -65,15 +114,14 @@ public class core {
     static MalFunction malDivide = new MalFunction() {
             @Override
             public MalInt apply(MalList args) throws MalException {
-                int size = args.getJValue().size();
+                assertNArgs(args, 1);
 
-                if (size == 0) throw new MalException("Wrong number of arguments: required >1, received 0.");
-
-                int result = checkMalInt(args.get(0));
+                int result = args.get(0).assertType(MalInt.class).getJValue();
+                int size = args.size();
                 if (size == 1) return new MalInt(1/result); // These are integers, so this will always return 0.
 
                 for (MalType i : args.getJValue().subList(1,size)) {
-                    result /= checkMalInt(i);
+                    result /= i.assertType(MalInt.class).getJValue();
                 }
                 return new MalInt(result);
             }
