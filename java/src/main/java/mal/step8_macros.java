8f7db9a61d77e8ed3e6f4ce82ab2941d1b69c908
Joost Kremers <joostkremers@fastmail.fm>
2018-10-19 10:33:44 +0200

    Implement cond and or.

--- java/src/main/java/mal/step8_macros.java
+++ java/src/main/java/mal/step8_macros.java
@@ -45,6 +45,8 @@ public class step8_macros {
         // Define `not' and `load-file'.
         try {
             rep("(def! not (fn* (a) (if a false true)))");
+            rep("(defmacro! or (fn* (& xs) (if (empty? xs) nil (if (= 1 (count xs)) (first xs) `(let* (or_FIXME ~(first xs)) (if or_FIXME or_FIXME (or ~@(rest xs))))))))");
+            rep("(defmacro! cond (fn* (& xs) (if (> (count xs) 0) (list 'if (first xs) (if (> (count xs) 1) (nth xs 1) (throw \"odd number of forms to cond\")) (cons 'cond (rest (rest xs)))))))" );
             rep("(def! load-file (fn* (f) (eval (read-string (str \"(do \" (slurp f) \")\")))))");
         } catch(MalException ex) {
             System.out.println("Internal error. Aborting.");
