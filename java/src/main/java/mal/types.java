joostkremers/jmal

commit f96cc03b6d725f294394869432d34accbb01c0e1

Mal MalNil a subclass of MalList.

java/src/main/java/mal/types.java
@@ -162,6 +162,21 @@ public class types {
         }
     }
 
+    private static class MalNil extends MalList {
+
+        public MalNil() {
+            super();
+            type = "symbol";
+        }
+
+        @Override
+        public String pr_str(boolean readably) {
+            return "nil";
+        }
+    }
+
+    public static final MalNil Nil = new MalNil();
+
     public static class MalVector extends MalSequence {
         public MalVector() {
             this.jValue = new ArrayList<MalType>();
@@ -311,25 +326,6 @@ public class types {
             return jValue;
         }
     }
-
-    private static class MalNil extends MalType {
-
-        public MalNil() {
-            type = "symbol";
-        }
-
-        @Override
-        public Boolean getJValue() {
-            return false;
-        }
-
-        @Override
-        public String pr_str(boolean readably) {
-            return "nil";
-        }
-    }
-
-    public static final MalNil Nil = new MalNil();
 
     private static class MalBoolean extends MalType {
         Boolean jValue;

commit 22bbe2237e5b871ce3528a428c49435f0b67ad5b

Let MalNil inherit from MalType instead of MalList.
This reverses f96cc03b6d725f294394869432d34accbb01c0e1, which caused `(= () nil)'
to return true, although it should return false.

java/src/main/java/mal/types.java
@@ -168,7 +168,7 @@ public class types {
         }
     }
 
-    private static class MalNil extends MalList {
+    private static class MalNil extends MalType {
 
         public MalNil() {
             super();
@@ -178,6 +178,11 @@ public class types {
         @Override
         public String pr_str(boolean readably) {
             return "nil";
+        }
+
+        @Override
+        public Object getJValue() {
+            return null;
         }
     }
 